fdce1991e56811cc8e924d2480c4432fe53c4312
Chia Yeng Xiong <chiayengxiong@Chias-MacBook-Pro.local>
2020-11-19 16:32:09 -0600

    Sign up component created.

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Router } from "@reach/router";
 import HomePage from "./Pages/HomePage";
 import TestPage from "./Pages/TestPage";
+import SignUpPage from "./Pages/SignUpPage";
 
 export default function App() {
   return (
     <Router>
-      <HomePage path="/" />
-      <TestPage path="/test" />
+      <HomePage path='/' />
+      <TestPage path='/test' />
+      <SignUpPage path='/sign-up' />
     </Router>
   );
 }
--- src/client/Pages/HomePage.jsx
+++ src/client/Pages/HomePage.jsx
@@ -4,7 +4,7 @@ import Layout from "../Components/Layout";
 import { makeStyles } from "@material-ui/core/styles";
 import { Link as BrowserLink } from "@reach/router";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   homePage: {
     height: "100%",
   },
@@ -24,29 +24,27 @@ export default function HomePage() {
       <Container className={classes.homePage}>
         <Grid
           container
-          justify="flex-end"
-          alignItems="center"
-          direction="column"
-        >
-          <Typography variant="h1">Kodning</Typography>
-          <Typography variant="subtitle1">
+          justify='flex-end'
+          alignItems='center'
+          direction='column'>
+          <Typography variant='h1'>Kodning</Typography>
+          <Typography variant='subtitle1'>
             The central place for coders to thrive. Post something to your story
             to share with all your friends!
           </Typography>
           <Grid
             container
-            justify="center"
+            justify='center'
             spacing={3}
-            className={classes.actionButtons}
-          >
+            className={classes.actionButtons}>
             <Grid item>
-              <BrowserLink to="/test" className={classes.link}>
-                <Button variant="contained">Sign In</Button>
+              <BrowserLink to='/test' className={classes.link}>
+                <Button variant='contained'>Sign In</Button>
               </BrowserLink>
             </Grid>
             <Grid item>
-              <BrowserLink to="/test" className={classes.link}>
-                <Button variant="contained">Sign Up</Button>
+              <BrowserLink to='/sign-up' className={classes.link}>
+                <Button variant='contained'>Sign Up</Button>
               </BrowserLink>
             </Grid>
           </Grid>
