d37db77dd8bbce59ffe573b49389b8dc6be62980
Timothy Casey <tvcasey@yahoo.com>
2020-11-18 19:45:11 -0500

    Adding more code to our auth code.  Things are taking some time.  Spent the last hour on getting my server to run.

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const os = require("os");
 
 const app = express();
-const { auth } = require('../api/auth');
+//const { auth } = require('../api/auth');
 
 app.use(express.static("dist"));
 app.get("/api/getUsername", (req, res) =>
--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -2,6 +2,9 @@ const Joi = require('joi');
 const bcrypt = require('bcrypt');
 const express = require('express');
 const { User } = require('../models/user');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+
 const router = express.Router();
 
 router.post('/', async (req, res) => {
@@ -16,7 +19,9 @@ router.post('/', async (req, res) => {
 
         if (!validPassword) return res.status(400).send('Invalid email or password.')
 
-        return res.send(true);
+        const token = jwt.sign({ _id: user._id, name: user.name }, config.get('jwtSecret'));
+
+        return res.send(token);
 }       catch (ex) {
         return res.status(500).send(`Internal Server Error: ${ex}`);
         }
