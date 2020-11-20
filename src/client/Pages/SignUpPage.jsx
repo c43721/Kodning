5eea0b33489e25c25e0f2458ea07451148e2ee58
Chia Yeng Xiong <chiayengxiong@Chias-MacBook-Pro.local>
2020-11-19 19:05:14 -0600

    adjust styles.

--- src/client/Pages/SignUpPage.jsx
+++ src/client/Pages/SignUpPage.jsx
@@ -1,21 +1,17 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
 
 function Copyright() {
   return (
-    <Typography variant='body2' color='textSecondary' align='center'>
+    <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
-      <Link color='inherit' href='https://material-ui.com/'>
+      <Link color="inherit" href="https://material-ui.com/">
         Kodning
       </Link>{" "}
       {new Date().getFullYear()}
@@ -25,123 +21,113 @@ function Copyright() {
 }
 
 const useStyles = makeStyles(theme => ({
-  splitScreen: {
-    display: "flex",
-    flexDirection: "row",
+  forms: {
+    width: "100",
+    marginBottom: theme.spacing(2),
+    paddingRight: theme.spacing(4),
+    paddingLeft: theme.spacing(4),
   },
-  paper: {
-    marginTop: theme.spacing(8),
-    width: "50%",
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-    borderRadius: "20px",
-  },
-  img: {
-    width: "50%",
-  },
+  submit: {},
 }));
 
 export default function SignUp() {
   const classes = useStyles();
 
   return (
-    <Container component='main' minWidth='xs' className='splitScreen'>
-      <Grid classname={classes.img}>
+    <Grid container>
+      <Grid item xs="6">
         <img
-          src='https://img.freepik.com/free-vector/programmer-s-workplace-writing-code-laptop_80328-238.jpg?size=626&ext=jpg'
-          alt='coding image'
+          src="https://img.freepik.com/free-vector/programmer-s-workplace-writing-code-laptop_80328-238.jpg?size=626&ext=jpg"
+          alt="coding image"
         />
       </Grid>
-      <div className={classes.paper}>
-        <Typography component='h1' variant='h5'>
-          Sign up
-        </Typography>
-        <form className={classes.form} noValidate>
-          <Grid>
+      <Grid item xs="6">
+        <div>
+          <Typography component="h1" variant="h5" className={classes.forms}>
+            Sign up
+          </Typography>
+          <form noValidate>
             <Grid>
-              <TextField
-                autoComplete='fname'
-                name='firstName'
-                variant='outlined'
-                required
-                fullWidth
-                id='firstName'
-                label='First Name'
-                autoFocus
-              />
+              <Grid className={classes.forms}>
+                <TextField
+                  autoComplete="fname"
+                  name="firstName"
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid className={classes.forms}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="lname"
+                />
+              </Grid>
+              <Grid item className={classes.forms}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item className={classes.forms}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+              </Grid>
+              <Grid item className={classes.forms}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="confirmPassword"
+                  label="Confrim Password"
+                  type="confirmPassword"
+                  id="confirmPassword"
+                  autoComplete="current-password"
+                />
+              </Grid>
             </Grid>
-            <Grid>
-              <TextField
-                variant='outlined'
-                required
-                fullWidth
-                id='lastName'
-                label='Last Name'
-                name='lastName'
-                autoComplete='lname'
-              />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}>
+              Sign Up
+            </Button>
+            <Grid container justify="center">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
             </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant='outlined'
-                required
-                fullWidth
-                id='email'
-                label='Email Address'
-                name='email'
-                autoComplete='email'
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant='outlined'
-                required
-                fullWidth
-                name='password'
-                label='Password'
-                type='password'
-                id='password'
-                autoComplete='current-password'
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant='outlined'
-                required
-                fullWidth
-                name='confirmPassword'
-                label='Confrim Password'
-                type='confirmPassword'
-                id='confirmPassword'
-                autoComplete='current-password'
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type='submit'
-            fullWidth
-            variant='contained'
-            color='primary'
-            className={classes.submit}>
-            Sign Up
-          </Button>
-          <Grid container justify='flex-end'>
-            <Grid item>
-              <Link href='#' variant='body2'>
-                Already have an account? Sign in
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
+          </form>
+        </div>
+      </Grid>
       <Box mt={5}>
         <Copyright />
       </Box>
-    </Container>
+    </Grid>
   );
 }
