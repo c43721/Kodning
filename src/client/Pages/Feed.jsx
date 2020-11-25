c0f062ab7c49faa5e37056c7b69bea9d0decb265
Chia Yeng Xiong <chiayengxiong@Chias-MacBook-Pro.local>
2020-11-25 14:12:06 -0600

    fixed style to feed page

--- src/client/Pages/Feed.jsx
+++ src/client/Pages/Feed.jsx
@@ -3,8 +3,8 @@ import React from "react";
 import Layout from "../Components/Layout";
 import { makeStyles } from "@material-ui/core/styles";
 import { Link as BrowserLink } from "@reach/router";
-import Card from "../Components/Card";
-import TextField from "../Components/TextField";
+import Post from "../Components/Post";
+import PostForm from "../Components/PostForm";
 
 const useStyles = makeStyles(theme => ({}));
 
@@ -19,8 +19,10 @@ export default function Feed({ onSubmit }) {
           justify="flex-end"
           alignItems="center"
           direction="column">
-          <TextField onSubmit={onSubmit} />
-          <Card />
+          <PostForm onSubmit={onSubmit} />
+          <Post />
+          <Post />
+          <Post />
         </Grid>
       </Container>
     </Layout>
