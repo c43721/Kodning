55103cae9119a3b54fd211a00ead96aa1f450a65
ajaxCodes <ajaxcodes@ajaxcodess-imac.home>
2020-11-25 20:22:01 -0600

    deleted friendspage.css, created begining of card component

--- src/client/Pages/FriendsPage.jsx
+++ src/client/Pages/FriendsPage.jsx
@@ -3,27 +3,33 @@ import Layout from "../Components/Layout";
 import Grid from "@material-ui/core/Grid";
 
 export default function FriendsPage(props) {
-	const friendArray = [
-		{
-			name: "1"
-		}
-	];
+  const friendArray = [
+    {
+      name: "1",
+    },
+  ];
 
-	return (
-		<Layout>
-			{friendArray.length ? (
-				<div className="friendsCenter">
-					{friendArray.map(friend => (
-						<Friend {...friend} />
-					))}
-				</div>
-			) : (
-				<p>No friends</p>
-			)}
-		</Layout>
-	);
+  return (
+    <Layout>
+      <Grid container>
+        {friendArray.length ? (
+          <>
+            {friendArray.map((friend) => (
+              <Friend {...friend} />
+            ))}
+          </>
+        ) : (
+          <Grid item>No friends</Grid>
+        )}
+      </Grid>
+    </Layout>
+  );
 }
 
 function Friend(props) {
-	return <Grid container>{props.name}</Grid>;
+  return (
+    <Grid item>
+      <Card>{props.name}</Card>
+    </Grid>
+  );
 }
