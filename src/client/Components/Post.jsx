6b2a832c41cd75e0345a976e2ac9d8607614220b
c43721 <rlahman.game@gmail.com>
2020-12-02 13:39:23 -0600

    Fix friends showing incorrect avatar

--- src/client/Components/Post.jsx
+++ src/client/Components/Post.jsx
@@ -42,18 +42,15 @@ export default function Post(props) {
 	const { user } = useUser();
 
 	const isOwnPost = user._id === props.user;
-	
+
 	return (
 		<Card className={classes.root}>
 			<CardHeader
 				title={props.authorData.username}
 				subheader={moment(props.date).format("MMMM Do YYYY, h:mm:ss a")}
 				avatar={
-					<Avatar
-						src={props.authorData.avatar.length > 2 ? `data:image/png;base64,${props.authorData.avatar}` : props.authorData.avatar}
-						className={classes.avatar}
-					>
-						{props.authorData.avatar.length > 2 ? `data:image/png;base64,${props.authorData.avatar}` : props.authorData.avatar}
+					<Avatar src={props.authorData.avatar} className={classes.avatar}>
+						{props.authorData.avatar}
 					</Avatar>
 				}
 				action={
